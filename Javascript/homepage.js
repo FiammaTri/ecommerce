2df9a73a9d7fa4267bcad8de88f03f79ceee71af
FiammaTri <scinty-pcd@live.it>
2025-02-05 18:05:39 +0100

    lavoro su home + fix header e footer

--- Javascript/homepage.js
+++ Javascript/homepage.js
@@ -1,4 +1,4 @@
-fetch('https://fakestoreapi.com/products/1') 
+fetch('https://fakestoreapi.com/products/15') 
             .then(res=>res.json())
             .then(json=>{console.log(json)
                 const card1 = document.getElementById('col-pref-donna')
@@ -6,11 +6,60 @@ fetch('https://fakestoreapi.com/products/1')
                 const titolo = document.querySelector('#col-pref-donna h5')
                 console.log(titolo);
                 titolo.innerHTML = json.title;
-                const desc = document.querySelector('#col-pref-donna p')
-                desc.innerHTML = json.description;
+                //const desc = document.querySelector('#col-pref-donna p')
+                //desc.innerHTML = json.description;
                 const prezzo = document.querySelector('#col-pref-donna span')
                 prezzo.innerHTML = json.price;
                 const immagine = document.querySelector('#col-pref-donna img')
+                immagine.src = json.image;
+            })
+
+            fetch('https://fakestoreapi.com/products/4') 
+            .then(res=>res.json())
+            .then(json=>{console.log(json)
+                const card1 = document.getElementById('col-pref-uomo')
+                console.log(card1);
+                const titolo = document.querySelector('#col-pref-uomo h5')
+                console.log(titolo);
+                titolo.innerHTML = json.title;
+                //const desc = document.querySelector('#col-pref-uomo p')
+                //desc.innerHTML = json.description;
+                const prezzo = document.querySelector('#col-pref-uomo span')
+                prezzo.innerHTML = json.price;
+                const immagine = document.querySelector('#col-pref-uomo img')
+                immagine.src = json.image;
+            })
+            
+            fetch('https://fakestoreapi.com/products/6') 
+            .then(res=>res.json())
+            .then(json=>{console.log(json)
+                const card1 = document.getElementById('col-pref-gioielli')
+                console.log(card1);
+                const titolo = document.querySelector('#col-pref-gioielli h5')
+                console.log(titolo);
+                titolo.innerHTML = json.title;
+                //const desc = document.querySelector('#col-pref-gioielli p')
+                //desc.innerHTML = json.description;
+                const prezzo = document.querySelector('#col-pref-gioielli span')
+                prezzo.innerHTML = json.price;
+                const immagine = document.querySelector('#col-pref-gioielli img')
+                immagine.src = json.image;
+            })
+
+
+            fetch('https://fakestoreapi.com/products/11') 
+            .then(res=>res.json())
+            .then(json=>{console.log(json)
+                const card1 = document.getElementById('col-pref-elettro')
+                console.log(card1);
+                const titolo = document.querySelector('#col-pref-elettro h5')
+                console.log(titolo);
+                titolo.innerHTML = json.title;
+                //const desc = document.querySelector('#col-pref-elettro p')
+                //desc.innerHTML = json.description;
+                const prezzo = document.querySelector('#col-pref-elettro span')
+                prezzo.innerHTML = json.price;
+                const immagine = document.querySelector('#col-pref-elettro img')
                 immagine.src = json.image;
             })
 

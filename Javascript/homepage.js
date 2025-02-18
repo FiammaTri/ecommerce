811ef30defc5bdd22f7ae46c335f3d29c1eb3dfb
FiammaTri <scinty-pcd@live.it>
2025-02-18 14:48:36 +0100

    fix home

--- Javascript/homepage.js
+++ Javascript/homepage.js
@@ -75,7 +75,7 @@ fetch('https://fakestoreapi.com/products/categories')
             const nav = document.createElement("div")
             nav.classList.add("col-6", "col-md-3", "col-cat")
             const link = document.createElement("a")
-            link.href = "Categoria.html?category=" + category
+            link.href = "../Categoria.html?category=" + category;
             const img = document.createElement("img")
             img.classList.add("img-fluid")
             img.alt = category
@@ -84,23 +84,23 @@ fetch('https://fakestoreapi.com/products/categories')
             const p = document.createElement("p")
 
             if (category == "women's clothing") {
-                img.src = "/Img/abbigliamento-donna.png"
+                img.src = "../Img/abbigliamento-donna.png"
                 p.innerHTML = "Abbigliamento donna"
             } else if (category == "men's clothing") {
-                img.src = "/Img/abbigliamento-uomo.png"
+                img.src = "../Img/abbigliamento-uomo.png"
                 p.innerHTML = "Abbigliamento uomo"
             } else if (category == "jewelery") {
-                img.src = "/Img/gioielli.png"
+                img.src = "../Img/gioielli.png"
                 p.innerHTML = "Gioielli"
             } else if (category == "electronics") {
-                img.src = "/Img/elettronica.png"
+                img.src = "../Img/elettronica.png"
                 p.innerHTML = "Elettronica"
             } else {
-                img.src = "/Img/abbigliamento-uomo.png"
+                img.src = "../Img/abbigliamento-uomo.png"
                 p.innerHTML = category
             }
+            console.log("Link creato: " + link.href);
             
-
             testo.appendChild(p);
             link.appendChild(img);
             link.appendChild(testo);
@@ -108,5 +108,6 @@ fetch('https://fakestoreapi.com/products/categories')
             navigazione.appendChild(nav);
 
         }
+        
 
     })
